f81e53ec29122a5bbb44264b744e1c58bd0a633a
Nightburnn <nightburn2003@gmail.com>
2023-02-28 06:00:17 -0800

    memeroy

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -14,7 +14,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	new = malloc(sizeof(binary_tree_t));
 	if (new == NULL)
-		return (NULL);// Failed to allocate memory
+		return (NULL);/* Failed to allocate memory*/
 
 	new->n = value;
 	new->parent = parent;
